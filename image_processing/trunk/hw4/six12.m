37327891583a1ff9a3f5e2ac0cb61a2eeed038c6
cshirk <cshirk@0d1693da-5620-0410-8003-e9b878f9e33f>
2006-10-28 23:29:17 +0000

    tmp

--- image_processing/trunk/hw4/six12.m
+++ image_processing/trunk/hw4/six12.m
@@ -1,2 +1,33 @@
 % hw #4, 6.12
+%
+% Chris Shirk (cshirk@ieee.org)
+%
+% Implement EBMA (Exhaustive Block Matching Algorithm) w/
+% block size 16x16
+%
+% Allow user to choose the search range (R)
+%
+
+close all;
+
+% Block size = 16x16
+blk_sz = [16 16];
+
+
+fid= fopen('foreman69.Y','r+','n'); 
+[Target_Image]= fread(fid,'uint8');
+fclose(fid);
+anchorFrame = reshape(Target_Image,352,288)'; 
+
+fid = fopen('foreman72.Y','r+','n');
+[Target_Image]= fread(fid,'uint8');
+fclose(fid);
+targetFrame = reshape(Target_Image,352,288)'; 
+
+subplot(2,1,1);
+imshow(anchorFrame/max(max(anchorFrame)));
+title('Anchor Frame');
+subplot(2,1,2);
+imshow(targetFrame/max(max(targetFrame)));
+title('Target Frame');
 
