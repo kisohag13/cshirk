aa76efbeb4eae1977bc439b5cbb25da7bf23cb2a
cshirk <cshirk@0d1693da-5620-0410-8003-e9b878f9e33f>
2006-11-06 16:51:58 +0000

    About to submit HW #4

--- image_processing/trunk/hw4/six15.m
+++ image_processing/trunk/hw4/six15.m
@@ -53,8 +53,7 @@ function six15(R)
     end
     
     % Perhaps we need to copy that image metadata again
-    predictedFrame = anchorFrame;
-    %predictedFrame(1:h, 1:w, 1:d) = 0;
+    predictedFrame = targetFrame;
     
     %%% Assume frame dimensions > given block size
     %%% Assume frame dimensions multiple of given block size
@@ -267,13 +266,136 @@ function six15(R)
     end
     
     
-    %%% Level 3: 352 x 288
+    %%% Level 3: half-pel search %%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % Double the x,y dimensions of anchor frame so that we can do half-pel
+    % search
+    anchorFrame = imresize(anchorFrame, 2, 'bilinear');
+    tmp_sz = size(anchorFrame);
+    disp(sprintf('Resized Anchor to %d x %d', tmp_sz(2), tmp_sz(1)));
+    
+    
+     % iteriate across the Target Frame blocks
+    % assume 140 px wide, 20 px wide blocks, 7 blocks...
+    % spanning 1:20, 21:40, 41:60, 61:80, 81:100, 101:120, 121:140
+    num_blks_y = (h / blk_sz);
+    num_blks_x = (w / blk_sz);
+    
+    R = 1; % only do half-pel correction
+    
+    %%% Iteriate across the Target Frame blocks...
+    for blk_y=1:1:num_blks_y
+        for blk_x=1:1:num_blks_x
+            
+            % Convert target block into origin coordinates
+            start_y = (blk_y - 1) * blk_sz + 1;
+            end_y = start_y + blk_sz - 1;
+            start_x = (blk_x - 1) * blk_sz + 1;
+            end_x = start_x + blk_sz - 1;
+            
+            % sprintf('start_y = %d, end_y = %d, start_x = %d, end_x = %d', start_y, end_y, start_x, end_x)
+            
+            % Init the min error to infinity...
+            % any error we actually get will be smaller
+            min_error = +inf;
+            
+            % Get coordinates of scaled anchor frame
+            % orig: 1 ==> 1
+            % orig: 2 ==> 3
+            % orig: 3 ==> 5
+            % orig: 4 ==> 7
+            r_y_from = max(2 * start_y - R - 1, 1);
+            r_y_to = min(2 * start_y + R - 1, 2 * (h - blk_sz));
+            
+            r_x_from = max(2 * start_x - R - 1, 1);
+            r_x_to = min(2 * start_x + R - 1, 2 * (w - blk_sz));
+            
+            for r_y = r_y_from:r_y_to 
+           
+                for r_x = r_x_from:r_x_to
+                
+                    % Ok, entire block within bounds
+                    % Now compute the error
+                    
+                    try
+                        error_sum = sum(sum( ...
+                            abs( ...
+                                anchorFrame(r_y:2:r_y+2 * blk_sz-1, r_x:2:r_x+2 * blk_sz-1, 1:d) - ...
+                                targetFrame(start_y:end_y, start_x:end_x, 1:d) ...
+                            ) ...
+                        ));
+                
+                    catch
+                        
+                        size(anchorFrame)
+                    
+                        disp(sprintf('anchorFrame(%d:%d, %d:%d)', r_y, r_y+2*blk_sz-1, r_x, r_x+2*blk_sz-1));
+                        disp(sprintf('targetFrame(%d:%d, %d:%d)', start_y, end_y, start_x, end_x));
+                       
+                        error 'oy'
+                    
+                    end
+                    
+                   
+                    
+                    %sprintf('min_error = %d, sum = %d', min_error, sum)
+                    if (error_sum < min_error)
+                        
+                        min_error = error_sum;
+
+                        best_r_x = r_x;
+                        best_r_y = r_y;
+                        best_del_x = r_x - 2 * start_x;
+                        best_del_y = r_y - 2 * start_y;
+                        
+                    end
+                    
+                end
+            end
+            
+            %disp(sprintf('blk x,y (%d,%d) -- from anchor x,y (%d,%d) to target x,y (%d,%d)', ...
+            %    blk_x, blk_y, best_r_x, best_r_y, start_x, start_y));
+            
+            % Save motion vectors, make them negative because our
+            % perspective is changing...
+            mv_y(blk_y, blk_x) = -best_del_y;
+            mv_x(blk_y, blk_x) = -best_del_x;
+            
+            % super lazy
+            offset_y(blk_y, blk_x) = best_r_y + blk_sz/2;
+            offset_x(blk_y, blk_x) = best_r_x + blk_sz/2;
+            
+        end
+    
+        drawnow();
+        
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%% Finalize %%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     
     % Plot predicted image
     subplot(2,2,4);
     imshow(predictedFrame/max(max(predictedFrame)));
     title('Predicted Image');
+    
+    
+    % Plot prediction-error image
+    % Scaled difference image: abs(2*(predicted image - target frame)+128) 
+    subplot(2,2,3);
+    scaledDiff = abs(2*(predictedFrame - targetFrame) + 128);
+    imshow(scaledDiff/max(max(scaledDiff)));
+    title('Scaled difference image');
+    hold on;
     
     
     
